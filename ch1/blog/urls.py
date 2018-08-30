1b0d688d757879e404e8f23ca23ecc7a11392e8c
yoongyo <jyg0172@naver.com>
2018-08-30 20:08:48 +0900

    2

--- ch1/blog/urls.py
+++ ch1/blog/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^blog/$', views.blog, name='blog'),
     url(r'^$', views.category_list, name='category_list'),
     url(r'^(?P<category>[\w|\W]+)/(?P<title>[\w|\W]+)/Edit/$', views.post_edit, name='post_edit'),
     url(r'^(?P<category>[\w|\W]+)/(?P<title>[\w|\W]+)/$', views.post_detail, name='post_detail'),
--- ch1/blog/views.py
+++ ch1/blog/views.py
@@ -61,26 +61,5 @@ def post_edit(request, category, title):
         'form': form,
     })
 
-def blog(request):
-    print('dddd')
-    import openpyxl
-    from selenium import webdriver as wd
-    import time
-
-    blog_list = []
-
-    wb = openpyxl.load_workbook('list.xlsm')
-
-    ws = wb.active
-    for i in ws.rows:
-        blog_list.append(i[1].value)
-
-    driver = wd.Chrome('chromedriver.exe')
-
-    driver.get('https://www.naver.com')
-    time.sleep(60)
-
-    for i in blog_list:
-        driver.get(i)
 
 
